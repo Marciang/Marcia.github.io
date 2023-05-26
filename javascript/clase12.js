8edaa454609c15733c77017bee2f5a9ddaeac5cd
Marciang <marciang.79@gmail.com>
2023-05-26 02:40:08 -0300

    Clase12
    Js Clase12

---
+++ javascript/clase12.js
@@ -0,0 +1,11 @@
+let nombre= prompt ("Por favor ingrese su nombre");
+function saludar (){
+    
+    if(nombre==""){
+        alert ("¡Por favor ingrese su nombre!");
+           }
+           else{
+            alert("¡Bienvenido,"+ nombre +"!");
+           }
+    }
+    console.log (saludar());
